16e8da565d14b2977eaa5cfbfcdc7492923de9bf
denis.soares.moreira@gmail.com <denis.soares.moreira@gmail.com@7c1b21b3-c80c-88aa-4108-3d0ffd8367ff>
2012-08-28 01:57:26 +0000

    Enquete

---
+++ Quets/src/main/java/br/com/quets/entity/Comentario.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.quets.entity;
+
+import java.util.Date;
+
+/**
+ *
+ * @author Guilhe
+ */
+public class Comentario {
+    
+    private Long codigo;
+    private Date criacao;
+    private String comentario;
+    
+}
