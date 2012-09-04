d125fa1a4c3594aec39343b96978a778e11474d0
denis.soares.moreira@gmail.com <denis.soares.moreira@gmail.com@7c1b21b3-c80c-88aa-4108-3d0ffd8367ff>
2012-09-04 01:05:34 +0000

    Prototipo

---
+++ Prototipo/src/java/web/login/view/AlterarSenha.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package web.login.view;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+/**
+ *
+ * @author dmoreira
+ */
+@ManagedBean(name="alterar")
+@RequestScoped
+public class AlterarSenha {
+    
+   
+}
---
+++ Prototipo/src/java/web/login/view/RecuperarSenha.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package web.login.view;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+/**
+ *
+ * @author dmoreira
+ */
+@ManagedBean(name = "recuperar")
+@RequestScoped
+public class RecuperarSenha {
+    
+    
+}
