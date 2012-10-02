123c309fcc7186c4a1e9f2918c7152ac896af451
guilhermedavidelucca <guilhermedavidelucca@7c1b21b3-c80c-88aa-4108-3d0ffd8367ff>
2012-10-02 16:06:46 +0000

    erro

--- trunk/Quets/src/main/java/br/com/quets/entity/Alternativa.java
+++ trunk/Quets/src/main/java/br/com/quets/entity/Alternativa.java
@@ -14,4 +14,62 @@ public class Alternativa {
     private Integer numeroAlternativa;
     private Integer numeroMinimo;
     private Integer numeroMaximo;
+
+    /**
+     * @return the codigo
+     */
+    public Long getCodigo() {
+        return codigo;
+    }
+
+    /**
+     * @param codigo the codigo to set
+     */
+    public void setCodigo(Long codigo) {
+        this.codigo = codigo;
+    }
+
+    /**
+     * @return the numeroAlternativa
+     */
+    public Integer getNumeroAlternativa() {
+        return numeroAlternativa;
+    }
+
+    /**
+     * @param numeroAlternativa the numeroAlternativa to set
+     */
+    public void setNumeroAlternativa(Integer numeroAlternativa) {
+        this.numeroAlternativa = numeroAlternativa;
+    }
+
+    /**
+     * @return the numeroMinimo
+     */
+    public Integer getNumeroMinimo() {
+        return numeroMinimo;
+    }
+
+    /**
+     * @param numeroMinimo the numeroMinimo to set
+     */
+    public void setNumeroMinimo(Integer numeroMinimo) {
+        this.numeroMinimo = numeroMinimo;
+    }
+
+    /**
+     * @return the numeroMaximo
+     */
+    public Integer getNumeroMaximo() {
+        return numeroMaximo;
+    }
+
+    /**
+     * @param numeroMaximo the numeroMaximo to set
+     */
+    public void setNumeroMaximo(Integer numeroMaximo) {
+        this.numeroMaximo = numeroMaximo;
+    }
+    
+    
 }
